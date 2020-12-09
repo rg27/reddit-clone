6ca5c0e1c59f3425da9534eb1882daf1d11ce9d7
rg27 <gualvezreymar@gmail.com>
2020-12-09 16:56:24 +0800

    modifying the timer 2

--- src/components/timer.js
+++ src/components/timer.js
@@ -18,19 +18,10 @@ const Timer = () => {
     };
      
     return (
-    //   <Countdown
-    //   date={new Date('2020-12-19T22:00:00').getTime() - Date.now()}
-    //   renderer={renderer}
-    // />
-    <div>
-      <iframe 
-        src="http://free.timeanddate.com/countdown/i7kzaid5/n1238/cf11/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cac000/cpc000/pct/tc66c/fs100/szw320/szh135/tac000/tpc000/mac000/mpc000/iso2020-12-19T22:00:00" 
-        allowTransparency="true" 
-        frameborder="0" 
-        width="290" 
-        height="50">
-      </iframe>
-    </div>
+      <Countdown
+      date={Date.now() + 500000}
+      renderer={renderer}
+    />
       )
     }
   
