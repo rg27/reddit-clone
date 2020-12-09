1ef90308206f6fc4576980c41ac6674e017b8d9f
rg27 <gualvezreymar@gmail.com>
2020-12-09 17:40:58 +0800

    timer

--- src/components/timer.js
+++ src/components/timer.js
@@ -18,10 +18,18 @@ const Timer = () => {
     };
      
     return (
-      <Countdown
-      date={Date.now() + 500000}
-      renderer={renderer}
-    />
+    //   <Countdown
+    //   date={Date.now() + 500000}
+    //   renderer={renderer}
+    // />
+    <iframe 
+    src="http://free.timeanddate.com/countdown/i7kzaid5/n1238/cf11/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cac000/cpc000/pct/tc66c/fs100/szw320/szh135/tac000/tpc000/mac000/mpc000/iso2020-12-19T22:00:00" 
+    allowTransparency="true" 
+    frameborder="0" 
+    width="290" 
+    height="50">
+  </iframe>
+    
       )
     }
   
