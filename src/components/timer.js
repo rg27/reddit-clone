ab0c9cfe4ee368b22942ba5253f9dfa38a200677
rg27 <gualvezreymar@gmail.com>
2020-12-10 12:14:33 +0800

    http to https

--- src/components/timer.js
+++ src/components/timer.js
@@ -9,7 +9,7 @@ const Timer = () => {
     return (
     <div>
       <Iframe 
-        src="http://free.timeanddate.com/countdown/i7kzaid5/n1238/cf11/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cac000/cpc000/pct/tc66c/fs100/szw320/szh135/tac000/tpc000/mac000/mpc000/iso2020-12-19T22:00:00" 
+        src="https://free.timeanddate.com/countdown/i7kzaid5/n1238/cf11/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cac000/cpc000/pct/tc66c/fs100/szw320/szh135/tac000/tpc000/mac000/mpc000/iso2020-12-19T22:00:00" 
         allowTransparency="true" 
         frameborder="0" 
         width="290" 
