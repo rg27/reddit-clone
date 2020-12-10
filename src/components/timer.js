ce5065f2e63d70dbab235719d9a8ee68de4a8495
rg27 <gualvezreymar@gmail.com>
2020-12-10 11:47:56 +0800

    Adding react-iframe

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,37 +1,21 @@
 import React from 'react';
+import Iframe from 'react-iframe'
 
-import Countdown from 'react-countdown';
- 
 // Random component
 
 
 const Timer = () => {
-    const Completionist = () => <span>You are good to go!</span>;
-    const renderer = ({ hours, minutes, seconds, completed }) => {
-      if (completed) {
-        // Render a completed state
-        return <Completionist />;
-      } else {
-        // Render a countdown
-        return <span>{hours} Days : {minutes} Minutes : {seconds} Seconds</span>;
-      }
-    };
      
     return (
-      <Countdown
-      date={Date.now() + 500000}
-      renderer={renderer}
-    />
-      // <iframe 
-      //     src="http://free.timeanddate.com/countdown/i7kzaid5/n1238/cf11/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cac000/cpc000/pct/tc66c/fs100/szw320/szh135/tac000/tpc000/mac000/mpc000/iso2020-12-19T22:00:00" 
-      //     allowTransparency="true" 
-      //     frameborder="0" 
-      //     width="290" 
-      //     height="50">
-      // </iframe>
-    
+    <div>
+      <Iframe 
+        src="http://free.timeanddate.com/countdown/i7kzaid5/n1238/cf11/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cac000/cpc000/pct/tc66c/fs100/szw320/szh135/tac000/tpc000/mac000/mpc000/iso2020-12-19T22:00:00" 
+        allowTransparency="true" 
+        frameborder="0" 
+        width="290" 
+        height="50"
+      />
+    </div>
       )
     }
-  
-
   export default Timer;
