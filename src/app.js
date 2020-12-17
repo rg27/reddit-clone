3e97ce4597081ce7420d1cff3cf614c4b63cccce
rg27 <gualvezreymar@gmail.com>
2020-12-17 13:50:50 +0800

    Adding background

--- src/app.js
+++ src/app.js
@@ -8,6 +8,11 @@ import Attendance from "./attendance";
 import Dashboard from "./dashboard";
 import ScrollIntoViewIfNeeded from 'react-scroll-into-view-if-needed';
 import Particles from 'react-particles-js';
+import xmas1 from './assets/images/xmas1.png'
+import xmas2 from './assets/images/xmas2.png'
+import xmas3 from './assets/images/xmas3.png'
+import xmas5 from './assets/images/xmas5.png'
+import xmas6 from './assets/images/xmas6.png'
 import {
   Col,
   Row,
@@ -19,28 +24,76 @@ const App = () => {
   return (
     <>
     <div className="app-background">
-          <Particles
-           style={{ position: "absolute" }}
-           height="95%"
-           width="95%"
-            params={{
-              "particles": {
-                  "number": {
-                      "value": 500
+
+<Particles
+ style={{ position: "absolute" }}
+ height="95%"
+ width="95%"
+    params={{
+	    "particles": {
+	        "number": {
+	            "value": 40,
+	            "density": {
+	                "enable": true,
+	                "value_area": 800
+	            }
+	        },
+	        "line_linked": {
+	            "enable": false
+	        },
+	        "move": {
+	            "speed": 2,
+	            "out_mode": "out"
+	        },
+	        "shape": {
+	            "type": [
+	                "image",
+	            ],
+	            "image": [
+	                {
+	                    "src": xmas1,
+	                    "height": 20,
+	                    "width": 23
                   },
-                  "size": {
-                      "value": 3
-                  }
-              },
-              "interactivity": {
-                  "events": {
-                      "onhover": {
-                          "enable": true,
-                          "mode": "repulse"
-                      }
-                  }
-              }
-          }} />
+                
+	                {
+	                    "src": xmas2,
+	                    "height": 20,
+	                    "width": 20
+	                },
+	                {
+	                    "src": xmas3,
+	                    "height": 20,
+	                    "width": 20
+                    },
+                    {
+	                    "src": xmas5,
+	                    "height": 20,
+	                    "width": 20
+                    },
+                    {
+	                    "src": xmas6,
+	                    "height": 20,
+	                    "width": 20
+	                }
+	            ]
+	        },
+	        "color": {
+	            "value": "#CCC"
+	        },
+	        "size": {
+	            "value": 40,
+	            "random": false,
+	            "anim": {
+	                "enable": true,
+	                "speed": 5,
+	                "size_min": 20,
+	                "sync": false
+	            }
+	        }
+	    },
+	    "retina_detect": false
+	}} />
       <Navbar />
       <Container>
           <Row>
