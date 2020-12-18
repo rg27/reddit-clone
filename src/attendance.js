7aed8915c596e77b9b5176188a7651cafd0471f7
rg27 <gualvezreymar@gmail.com>
2020-12-18 22:36:40 +0800

    Changing from sheet1 to Raw

--- src/attendance.js
+++ src/attendance.js
@@ -35,7 +35,7 @@ const handleSubmit = async (e) => {
   
   try{
     const response = await fetch(
-      "https://v1.nocodeapi.com/rey12/google_sheets/phNpAEVUJmlYKctq?tabId=Sheet1",
+      "https://v1.nocodeapi.com/rey12/google_sheets/phNpAEVUJmlYKctq?tabId=Raw",
       {
         method:'POST',
         headers:{
