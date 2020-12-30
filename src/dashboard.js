282062ad5470c5f00991f624d36f40175780ef5c
rg27 <gualvezreymar@gmail.com>
2020-12-30 15:32:54 +0800

    Making the width of dashbaord responsive

--- src/dashboard.js
+++ src/dashboard.js
@@ -13,7 +13,7 @@ return (
   <Card>
     <CardBody>
         <Iframe 
-            width="1060"
+            width="100%"
             height="800" 
             src="https://datastudio.google.com/embed/reporting/1358356b-8b4e-4c88-a13e-36578d3219a7/page/VgD" 
             frameborder="0" 
